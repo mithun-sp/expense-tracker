6e15097bd5471dbe83dd790fb8eb656df62829ce
Mithun S <mithunpillai007@gmail.com>
2023-10-31 00:27:48 +0530

    Added ExpenseItem

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return (
+    <div>
+      <h2>ExpenseItems</h2>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies Rs 200</p>
+    </div>
+  );
+};
+
+export default ExpenseItem;
