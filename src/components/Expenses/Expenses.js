3feea16a72ff489509b29f926ac941a9f72f6087
Mithun S <mithunpillai007@gmail.com>
2023-11-02 12:08:44 +0530

    Adding Event Listeners - delete expense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [data, setData] = useState(props.items);
+
+  const deleteExpense = (index) => {
+    let newData = data.filter((el) => {
+      return el.id !== index;
+    });
+    setData(newData);
+  };
+
   return (
     <Card className="expenses">
-      {props.items.map((item) => {
+      {data.map((item) => {
         return (
-          <ExpenseItem
-            title={item.title}
-            amount={item.amount}
-            location={item.location}
-            date={item.date}
-          />
+          <div>
+            <ExpenseItem
+              title={item.title}
+              amount={item.amount}
+              location={item.location}
+              date={item.date}
+            />
+            <button onClick={() => deleteExpense(item.id)}>
+              Delete Expense
+            </button>
+          </div>
         );
       })}
     </Card>
